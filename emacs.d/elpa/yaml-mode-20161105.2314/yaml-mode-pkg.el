644c163a0484e5c9043d8eb549527f2702f05318
Linusp <linusp1024@gmail.com>
2017-01-11 16:12:10 +0800

    add yaml-mode && remove compiled files

---
+++ emacs.d/elpa/yaml-mode-20161105.2314/yaml-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yaml-mode" "20161105.2314" "Major mode for editing YAML files" '((emacs "24.1")) 'url: "https://github.com/yoshiki/yaml-mode" :keywords '(" data yaml"))
