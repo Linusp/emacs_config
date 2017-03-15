cd35e9a40a9d9aa77367cb066e3e3e398120e081
Linusp <linusp1024@gmail.com>
2017-03-16 07:35:19 +0800

    add helm-swoop and org-download

---
+++ emacs.d/elpa/helm-swoop-20160620.53/helm-swoop-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-swoop" "20160620.53" "Efficiently hopping squeezed lines powered by m interface * coding: utf8 lexicalbinding: t *" '((emacs "24.3") (helm "1.0")) 'url: "https://github.com/ShingoFukuyama/helm-swoop" :keywords '(" helm swoop inner buffer search"))
---
+++ emacs.d/elpa/org-download-20170214.351/org-download-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-download" "20170214.351" "Image draganddrop for Emacs orgmode" '((async "1.2")) 'url: "https://github.com/abo-abo/org-download" :keywords '(" images" " screenshots" " download"))
