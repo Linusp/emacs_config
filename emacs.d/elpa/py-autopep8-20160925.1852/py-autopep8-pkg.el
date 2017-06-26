2d19cf7a6af66e75579c8586cccbe33588d01c04
Linusp <linusp1024@gmail.com>
2017-06-26 14:54:47 +0800

    add package: py-autopep8

---
+++ emacs.d/elpa/py-autopep8-20160925.1852/py-autopep8-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "py-autopep8" "20160925.1852" "Use autopep8 to beautify a Python buffer" 'nil 'url: "https://github.com/paetzke/py-autopep8.el" :keywords '(""))
