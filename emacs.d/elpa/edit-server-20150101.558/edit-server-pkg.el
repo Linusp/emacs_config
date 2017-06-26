cad053731d682e89fc536f2b30d59ed77fa79635
Linusp <linusp1024@gmail.com>
2017-06-26 14:30:38 +0800

    add package: edit-server

---
+++ emacs.d/elpa/edit-server-20150101.558/edit-server-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "edit-server" "20150101.558" "server that responds to edit requests from Chrome" 'nil 'url: "https://github.com/stsquad/emacs_chrome" :keywords '(""))
