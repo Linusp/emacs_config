61119ace70feca0d16bab880c465cd8c2434ad22
Linusp <linusp1024@gmail.com>
2017-08-14 07:18:06 +0800

    add yafolding

---
+++ emacs.d/elpa/yafolding-20170305.1117/yafolding-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yafolding" "20170305.1117" "Yet another folding extension for Emacs" 'nil 'url: "https://github.com/zenozeng/yafolding.el" :keywords '(""))
