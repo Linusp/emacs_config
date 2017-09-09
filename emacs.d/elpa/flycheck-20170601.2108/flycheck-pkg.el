6de314a1a5540a6c18e890bbe2a7f5c30ca9ebc2
Linusp <linusp1024@gmail.com>
2017-09-09 17:35:23 +0800

    add flycheck

---
+++ emacs.d/elpa/flycheck-20170601.2108/flycheck-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flycheck" "20170601.2108" "Onthefly syntax checking"  '((dash "2.12.1") (pkg-info "0.4") (let-alist "1.0.4") (seq "1.11") (emacs "24.3") ) :url "https://github.com/flycheck/flycheck" :keywords '("convenience" "languages" "tools" ))
