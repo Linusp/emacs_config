7738b56a4b02cceb80e5839a517db32268409e13
Linusp <linusp1024@gmail.com>
2017-09-16 17:25:06 +0800

    add packages for flycheck+virtualenv

---
+++ emacs.d/elpa/auto-virtualenvwrapper-20170618.1552/auto-virtualenvwrapper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "auto-virtualenvwrapper" "20170618.1552" "Lightweight auto activate python virtualenvs" '((s "1.10.0") (virtualenvwrapper "0") (cl-lib "0.6")) 'url: "https://github.com/robert-zaremba/auto-virtualenvwrapper.el" :keywords '(" Python" " Virtualenv" " Tools"))
---
+++ emacs.d/elpa/virtualenvwrapper-20161003.615/virtualenvwrapper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "virtualenvwrapper" "20161003.615" "a featureful virtualenv tool for Emacs" '((dash "1.5.0") (s "1.6.1")) 'url: "https://github.com/porterjamesj/virtualenvwrapper.el" :keywords '(" python" " virtualenv" " virtualenvwrapper"))
