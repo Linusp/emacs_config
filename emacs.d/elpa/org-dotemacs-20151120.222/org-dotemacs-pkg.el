3530ba0a18c31f86bf86d2c84328a5fe4fb26c40
Linusp <linusp1024@gmail.com>
2019-04-07 11:28:54 +0800

    use `org-babel-load-file` instead of `org-dotemacs`

--- emacs.d/elpa/org-dotemacs-20151120.222/org-dotemacs-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-dotemacs" "20151120.222" "Store your emacs config as an org file, and choose which bits to load." '((cl-lib "1.0") (org "7.9.3")) 'url: "https://github.com/vapniks/org-dotemacs" :keywords '(" local"))
