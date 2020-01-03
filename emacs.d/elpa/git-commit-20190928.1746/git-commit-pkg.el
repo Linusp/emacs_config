164caa1de4709312a4726ee1c21b11bffcf686a4
Linusp <linusp1024@gmail.com>
2020-01-03 12:09:04 +0800

    update package `magit` dependencies

---
+++ emacs.d/elpa/git-commit-20190928.1746/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190928.1746" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "8b3172fc495d83830573461f877ed390e6408e0b" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
